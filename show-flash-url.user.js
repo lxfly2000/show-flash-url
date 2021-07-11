735d10b5e63ffd565ada2b2194e5b2ae7d87a370
Yuxin <gaoboyuan8@qq.com>
2021-07-11 21:09:04 +0800

    Update show-flash-url.user.js

--- show-flash-url.user.js
+++ show-flash-url.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Show Flash URL
 // @namespace    https://github.com/lxfly2000/show-flash-url/raw/master/show-flash-url.user.js
-// @version      1.0.2
+// @version      1.0.4
 // @updateURL    https://github.com/lxfly2000/show-flash-url/raw/master/show-flash-url.user.js
 // @downloadURL  https://github.com/lxfly2000/show-flash-url/raw/master/show-flash-url.user.js
 // @description  显示网页中的Flash链接
@@ -19,19 +19,29 @@ function ShowFlashURL_StandarizeURL(str){
 
     var flashObjs=document.querySelectorAll("object[classid='clsid:D27CDB6E-AE6D-11cf-96B8-444553540000']");
     for(var i=0;i<flashObjs.length;i++){
+        var divLink=document.createElement("div");
         var linkText=document.createElement("a");
-        linkText.innerText=linkText.href=ShowFlashURL_StandarizeURL(flashObjs[i].querySelector("param[name=movie]").value);
-        console.log("发现Flash资源(ActiveX)：["+i+"]"+linkText);
-        flashObjs[i].parentElement.append(linkText);
+        divLink.append(linkText);
+        var param=flashObjs[i].querySelector("param[name=movie]").value;
+        if(param!=undefined&&param!=""){
+            linkText.innerText=linkText.href=ShowFlashURL_StandarizeURL(param);
+            console.log("发现Flash资源(ActiveX)：["+i+"]"+linkText);
+            flashObjs[i].parentElement.append(divLink);
+        }
     }
     //Non-IE case
     if(flashObjs.length==0){
         flashObjs=document.querySelectorAll("object[type='application/x-shockwave-flash']");
         for(i=0;i<flashObjs.length;i++){
+            divLink=document.createElement("div");
             linkText=document.createElement("a");
-            linkText.innerText=linkText.href=ShowFlashURL_StandarizeURL(flashObjs[i].data);
-            console.log("发现Flash资源(Plugin)：["+i+"]"+linkText);
-            flashObjs[i].parentElement.append(linkText);
+            divLink.append(linkText);
+            param=flashObjs[i].data;
+            if(param!=undefined&&param!=""){
+                linkText.innerText=linkText.href=ShowFlashURL_StandarizeURL(param);
+                console.log("发现Flash资源(Plugin)：["+i+"]"+linkText);
+                flashObjs[i].parentElement.append(divLink);
+            }
         }
     }
 })();
